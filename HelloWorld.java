987a74f9eb231a61a2132afad27304e0fe0761f3
AaronNgo <atngo106@student.hbuhsd.edu>
2024-09-03 11:21:02 -0700

    dab lol i did this

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,15 @@ public class HelloWorld
 {
   public static void main(String[] args)
   {
-    System.out.print("Hello world!");
+    System.out.println("Hello world!");
+    System.out.println("Hello World!");
+
+    int myAge;
+    myAge = 16;
+
+    double price = 4.99;  // set price to 4.99
+    String myName = "Aaron Ngo";
+
+    System.out.println("Hello " + myName + " it's nice to meet you");
   }
 }
--- problem1.java
+++ problem1.java
@@ -1,1 +1,9 @@
-
+public class problem1
+{
+  public static void main(String[] args)
+  {
+    System.out.println("skibidi toilet rizz");
+    System.out.println("ohio rizz");
+    System.out.println("hawk tuah!");
+  }
+}
